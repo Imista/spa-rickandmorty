9bd36b63a532603b4486a1f1ccf222ee38101930
MuffinPlease <benjamintec3455@gmail.com>
2022-01-13 19:13:00 -0600

    Webpack implementation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import setRoute from './routes/index.js';
+import './styles/styles.css'
 
 window.addEventListener('load', setRoute);
 window.addEventListener('hashchange', setRoute);
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_module/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use:[MiniCssExtractPlugin.loader, 
+                'css-loader'],
+            },
+        ],
+    },
+    devServer: {
+        static: 
+        {
+          directory: path.join(__dirname, "dist"),
+          watch: true,
+        },
+        watchFiles: path.join(__dirname, "./**"), 
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(),
+    ]
+}
